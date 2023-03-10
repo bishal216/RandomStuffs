f5f71c15b661fa756c16c8362785b56272842b32
Bishal Katuwal <bis3hal@gmail.com>
2023-03-10 19:27:46 +0545

    BigData
    Case Study

---
+++ Random Scripts/WEBPtoPNG.py
@@ -0,0 +1,12 @@
+import os
+from PIL import Image
+
+def convert_all_webp_to_png(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.webp'):
+            webp_path = os.path.join(folder_path, filename)
+            png_path = os.path.join(folder_path, filename[:-5] + '.png')
+            with Image.open(webp_path) as im:
+                im.save(png_path, 'png')
+            os.remove(webp_path)    
+convert_all_webp_to_png('BigData\\images')
